47ea93a788fcec2713b2a39b3b1b471ea2c103d7
cindrmon <cindrmon@protonmail.com>
2021-11-26 14:53:02 +0800

    add data display for user, product and cart

---
+++ src/com/raspi/retail/frontend/view/display/ProductDisplay.java
@@ -0,0 +1,45 @@
+package com.raspi.retail.frontend.view.display;
+
+import com.raspi.retail.backend.model.dtos.ProductDTO;
+
+import java.util.ArrayList;
+
+public class ProductDisplay {
+
+    public static void displayProducts(ArrayList<ProductDTO> products) {
+
+        if(products.size() > 0) {
+            products.forEach(product ->  {
+                    System.out.println("\n=============================================");
+                    System.out.println("[" + product.getId() + "]: " + product.getName());
+                    System.out.println("\"" + product.getDescription() + "\"");
+                    System.out.println();
+                    System.out.println("Type: " + product.getProductType());
+                    System.out.println("Price: USD " + String.format("%.2f", product.getPrice()));
+                    if ((product.getStock() > 0)) {
+                        System.out.println("Stock Remaining: " + product.getStock());
+                    } else {
+                        System.out.println("Product out of stock.");
+                    }
+                    System.out.println("URL: " + product.getUrlListing());
+                    System.out.println("=============================================\n");
+            });
+        } else {
+            System.out.println("No products exist.");
+        }
+
+    }
+
+    public static void displayProduct(ProductDTO product) {
+            System.out.println("\n=============================================");
+            System.out.println("[" + product.getId() + "]: " + product.getName());
+            System.out.println("\""+ product.getDescription() +"\"");
+            System.out.println();
+            System.out.println("Type: " + product.getProductType());
+            System.out.println("Price: USD " + String.format("%.2f", product.getPrice()));
+            System.out.println("Stock Remaining: " + product.getStock());
+            System.out.println("URL: " + product.getUrlListing());
+            System.out.println("=============================================\n");
+    }
+
+}
