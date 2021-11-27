d696d29f2cbb4ee7f0ca9053bf6df679d9ac0abe
cindrmon <cindrmon@protonmail.com>
2021-11-27 21:41:06 +0800

    fix cart dao guest query stores

--- src/com/raspi/retail/backend/model/daos/query/store/CartDAOGuestQueryStore.java
+++ src/com/raspi/retail/backend/model/daos/query/store/CartDAOGuestQueryStore.java
@@ -4,16 +4,16 @@ package com.raspi.retail.backend.model.daos.query.store;
  * SQL Queries for Guest Customer Carts
  */
 public enum CartDAOGuestQueryStore {
-    GET_USER_CART("SELECT * FROM `RasPiRetail_GuestsCart` WHERE `customerID`=?"),
+    GET_USER_CART("SELECT * FROM `RasPiRetail_GuestsCart` WHERE `guestID`=?"),
     GET_USER_CART_ITEM("SELECT * FROM `RasPiRetail_GuestCustomerCarts` " +
                 "WHERE `guestID`=? && `productID`=? "),
     POST_USER_CART_ITEM("INSERT INTO `RasPiRetail_GuestCustomerCarts` " +
-                "SET `customerID`=?, `productID`=?, `quantity`=?"),
+                "SET `guestID`=?, `productID`=?, `quantity`=?"),
     PUT_USER_CART_ITEM("UPDATE `RasPiRetail_GuestCustomerCarts` " +
                 "SET `quantity`=?, `dateUpdated`=CURRENT_TIMESTAMP " +
-                "WHERE `customerID`=? && `productID`=?"),
+                "WHERE `guestID`=? && `productID`=?"),
     DELETE_USER_CART_ITEM("DELETE FROM `RasPiRetail_GuestCustomerCarts` " +
-                "WHERE `customerID`=? && `productID`=? ");
+                "WHERE `guestID`=? && `productID`=? ");
 
     private String query;
 
